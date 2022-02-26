d46e64428cd683982f1c11696b5a2b106a2acd04
Ali Fakoor <ali.fakoor13@gmail.com>
2022-02-26 09:06:38 +0330

    add data by user - just Ammonia and Oxygen

--- app/DashExample/routes.py
+++ app/DashExample/routes.py
@@ -23,62 +23,62 @@ def app2_template():
 @blueprint.route('/add_data', methods=['GET', 'POST'])
 @login_required
 def add_data():
-    admin_user = current_app.config['ADMIN']['username']
-    if current_user.username == admin_user:
+    # admin_user = current_app.config['ADMIN']['username']
+    # if current_user.username == admin_user:
         form = AddDataForm(request.form, meta={'csrf': False})
         status = ''
         if request.method == 'POST':
             if form.validate():
-                temperature = {
-                    'value': request.form.get('temperature'),
-                    'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
-                    'user_id': current_user.id
-                }
-                ph = {
-                    'value': request.form.get('ph'),
-                    'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
-                    'user_id': current_user.id
-                }
-                ec = {
-                    'value': request.form.get('ec'),
-                    'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
-                    'user_id': current_user.id
-                }
-                orp = {
-                    'value': request.form.get('orp'),
-                    'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
-                    'user_id': current_user.id
-                }
+                # temperature = {
+                #     'value': request.form.get('temperature'),
+                #     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
+                #     'user_id': current_user.id
+                # }
+                # ph = {
+                #     'value': request.form.get('ph'),
+                #     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
+                #     'user_id': current_user.id
+                # }
+                # ec = {
+                #     'value': request.form.get('ec'),
+                #     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
+                #     'user_id': current_user.id
+                # }
+                # orp = {
+                #     'value': request.form.get('orp'),
+                #     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
+                #     'user_id': current_user.id
+                # }
                 ammonia = {
                     'value': request.form.get('ammonia'),
                     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
                     'user_id': current_user.id
                 }
-                nitrite = {
-                    'value': request.form.get('nitrite'),
-                    'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
-                    'user_id': current_user.id
-                }
-                nitrate = {
-                    'value': request.form.get('nitrate'),
-                    'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
-                    'user_id': current_user.id
-                }
+                # nitrite = {
+                #     'value': request.form.get('nitrite'),
+                #     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
+                #     'user_id': current_user.id
+                # }
+                # nitrate = {
+                #     'value': request.form.get('nitrate'),
+                #     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
+                #     'user_id': current_user.id
+                # }
                 oxygen = {
                     'value': request.form.get('oxygen'),
                     'timestamp': datetime.datetime.now(pytz.timezone('Asia/Tehran')),
                     'user_id': current_user.id
                 }
-                Temperature(**temperature).add_to_db()
-                PH(**ph).add_to_db()
-                EC(**ec).add_to_db()
-                ORP(**orp).add_to_db()
+                # Temperature(**temperature).add_to_db()
+                # PH(**ph).add_to_db()
+                # EC(**ec).add_to_db()
+                # ORP(**orp).add_to_db()
                 Ammonia(**ammonia).add_to_db()
-                Nitrite(**nitrite).add_to_db()
-                Nitrate(**nitrate).add_to_db()
+                # Nitrite(**nitrite).add_to_db()
+                # Nitrate(**nitrate).add_to_db()
                 Oxygen(**oxygen).add_to_db()
                 status = 'All Good.'
             else:
                 status = 'All field must be correct.'
         return render_template('add_data.html', form=form, status=status)
-    return redirect('/page_403')
+    # return redirect('/page_403')
